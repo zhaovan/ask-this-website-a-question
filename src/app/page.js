29d81b90ab6c1b3862b01903a876bf071f12e744
Ivan Zhao <ivanzhao8@gmail.com>
2024-12-12 10:51:12 -0800

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,50 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const questions = [
+  {
+    number: 1,
+    title: "Intuition",
+    text: "When you sit quietly with this question, can you trust your gut and internal compass?",
+  },
+  {
+    number: 2,
+    title: "Resources",
+    text: "Do you have the time and money to give this question the attention it deserves?",
+  },
+  {
+    number: 3,
+    title: "Skills",
+    text: "Do you have the skills and knowledge to navigate this question?",
+  },
+  {
+    number: 4,
+    title: "Support",
+    text: "When you talk to those who matter most – your friends, family, and community – do you feel supported and understood?",
+  },
+  {
+    number: 5,
+    title: "Emotions",
+    text: "Do you feel emotionally prepared to face whatever truths and challenges may emerge when answering this question?",
+  },
+  {
+    number: 6,
+    title: "Impact",
+    text: "Have you considered how your decision might impact the people around you, the environment, or the future?",
+  },
+];
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+export default function Home() {
+  const [question, setQuestion] = useState("");
+  const [questionResponses, setQuestionResponses] = useState("");
+  const [stage, setStage] = useState(0);
+  return (
+    <div>
+      <p>Should I </p>
+      <input type="text" />
+
+      <button>next</button>
     </div>
   );
 }
